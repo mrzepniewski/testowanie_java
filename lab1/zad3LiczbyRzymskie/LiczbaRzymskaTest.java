c1af7c42224c42f3dd6bf2390ea0f7f308db153b
mrzepniewski <mrzepniewski@sigma.ug.edu.pl>
2015-03-06 23:03:21 +0100

    changes

--- lab1/zad3LiczbyRzymskie/LiczbaRzymskaTest.java
+++ lab1/zad3LiczbyRzymskie/LiczbaRzymskaTest.java
@@ -1,47 +1,80 @@
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+
 import org.junit.Test;
 import org.junit.Assert;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
 import static org.hamcrest.BaseMatcher.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+
+import static org.junit.Assert.*;
+
+import static org.junit.Assert.*;
+
+import static org.junit.Assert.*;
+
+import static org.junit.Assert.*;
 
 public class LiczbaRzymskaTest {
 	
 	@Test
-	public void verifyRomanThat434(){
+	public void TestRomanThat434(){
 		LiczbaRzymska roman = new LiczbaRzymska(434);
 		assertThat("should be equal", roman.toString().equals("CDXXXIV"));
 	}
 	@Test
-	public void verifyRomanEquals434(){
+	public void TestRomanEquals434(){
 		LiczbaRzymska roman = new LiczbaRzymska(434);
 		assertEquals(true, roman.toString().equals("CDXXXIV"));
 	}
 	@Test
-	public void verifyRomanNotEquals(){
+	public void TestRomanEquals2(){
+		LiczbaRzymska roman = new LiczbaRzymska(2);
+		 String expectedArray = new String();
+		 expectedArray="II";
+		 assertEquals(expectedArray, roman.toString());
+		 
+	}
+	@Test
+	public void TestRomanWhatDoIfOutOfRange(){
+		LiczbaRzymska roman = new LiczbaRzymska(40000);
+		
+		 assertEquals(null, roman.toString());
+		 
+	}
+	
+	@Test
+	public void TestRomanNotEquals(){
 		LiczbaRzymska roman = new LiczbaRzymska(434);
 		assertNotEquals("should be equal", roman.toString().equals("CDXXX"));
 	}
 	@Test
-	public void verifyRomanNotNull(){
+	public void TestRomanNotNull(){
 		LiczbaRzymska roman = new LiczbaRzymska(3999);
 		assertNotNull("should not be null", roman.toString());
 	}
 	
 	@Test
-	public void verifyRomanAssertFalse(){
+	public void TestRomanAssertFalse(){
 		LiczbaRzymska roman = new LiczbaRzymska(4999);
 		assertFalse("failure - should be false", roman.toString()!=null);
 	}
 	@Test
-	public void verifyRomanNull(){
+	public void TestRomanNull(){
 		LiczbaRzymska roman = new LiczbaRzymska(4999);
 		assertNull("should be null", roman.toString());
 	}
 	@Test
-	public void verifyRomanShouldNotBeSame(){
+	public void Test1RomanShouldNotBeSame(){
 		LiczbaRzymska roman = new LiczbaRzymska(5);
 		assertNotSame("should not be same", roman.toString(),"VI");
+	}
+	@Test
+	public void TestRomanTrue(){
+		LiczbaRzymska roman = new LiczbaRzymska(5315);
+		assertTrue("should  be true", roman.toString()==null);
 	}
 	
 }
